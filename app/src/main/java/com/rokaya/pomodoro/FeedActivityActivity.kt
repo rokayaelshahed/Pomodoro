e811af699a27723a109595600e915c046238bca4
rokayaelshahed <RokayaElShahed33941@azhar.edu.eg>
2023-09-15 00:27:40 +0300

    Init commit

---
+++ app/src/main/java/com/rokaya/pomodoro/FeedActivityActivity.kt
@@ -0,0 +1,11 @@
+package com.rokaya.pomodoro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedActivityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.feed_activity)
+    }
+}
